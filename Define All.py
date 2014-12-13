2594c1a0722c49ee9f421be969b0b62796796ceb
Kurush Dubash <dubash.kurush@gmail.com>
2014-12-13 02:37:08 -0800

    Limited results to only 1, added except clause

--- Define All.py
+++ Define All.py
@@ -1,30 +1,22 @@
-# from xml.etree import ElementTree
-# from xmlutils.xml2json import xml2json
 import xmltodict, json
 import requests
 
 def get_def(word):
-	#url = 'http://www.dictionaryapi.com/api/v1/references/collegiate/xml/' + str(word) + '?key=3b0ab062-83f3-44dd-8a38-77641fc48664'
-	url = 'http://api.wordnik.com/v4/word.json/' + str(word).lower() + '/definitions?limit=200&includeRelated=true&sourceDictionaries=all&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
-	response = requests.get(url).text
-	response = json.loads(response)[0]['text']
-	definition = response
-
-
-	# doc = xmltodict.parse(response.text)
-	# data = json.loads(json.dumps(doc))
-	# print(json.dumps(data, sort_keys=True,indent=4, separators=(',', ': ')))
-
-	# definition = data['entry_list']['entry']['def']['dt'][1][1:].capitalize()
-
+	url = 'http://api.wordnik.com/v4/word.json/' + str(word) + '/definitions?limit=1&includeRelated=true&sourceDictionaries=all&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
+	try: 
+		response = requests.get(url).text
+		response = json.loads(response)[0]
+		definition = response['text']
+	except:
+		definition = 'Something went wrong : Could not find defintiion'
 	return definition
-	# print(definition)
-	# converter = xml2json(response.text)
-	# print (converter.get_json())
 
-	# data = ElementTree.fromstring(response.content)
 print("Input a list of words\n")
-word_list = input().split(', ')
+word_list = input()
+if ',' in word_list:
+	word_list=word_list.split(', ')
+else:
+	word_list=word_list.split()
 print('\n')
 for word in word_list:
-	print(word.capitalize() + " : " + get_def(word) + '\n') 
+	print(word + " : " + get_def(word) + '\n') 
